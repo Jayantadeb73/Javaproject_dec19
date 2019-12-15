edd01cacb71de576d03664120229104b531c9551
Jayantadeb73 <48243798+Jayantadeb73@users.noreply.github.com>
2019-12-15 14:35:22 +0000

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -5,6 +5,6 @@ public class AddTwoNumbers {
       int num1 = 5, num2 = 157, sum;
       sum = num1 + num2;
 
-      System.out.println("Hello Sum of these numbers: "+sum);
+      System.out.println("Hello how Sum of these numbers: "+sum);
    }
 }
